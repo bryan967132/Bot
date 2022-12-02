d38085460096f6ad1084f610d425c560d087c75f
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2022-12-01 23:00:31 -0600

    TestParser

--- TestScanner.py
+++
@@ -1,9 +0,0 @@
-from Scanner import Scanner
-lexical = Scanner()
-while True:
-    command = input('Ingrese un comando: ')
-    lexical.analyze(command)
-    lexical.printTokens()
-    lexical.printErrors()
-    if command == 'ADIOS':
-        break
