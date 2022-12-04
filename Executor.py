fa480a9122d7c9b8db1fda35d944122016b14f16
bryan967132 <2780289260101@ingenieria.usac.edu.gt>
2022-12-03 22:36:03 -0600

    Executor

--- Executor.py
+++ Executor.py
@@ -1,11 +1,17 @@
 from Scanner import Scanner
 from Parser import Parser
+from DataBase import DB,Request
 class Executor:
     def __init__(self):
         self.__lexical = Scanner()
         self.__syntax = Parser()
+        self.__db = DB()
+        self.__request = Request(self.__db)
 
     def getResponse(self,command):
         self.__lexical.analyze(command)
         self.__syntax.analyze(self.__lexical.getTokens())
-        print(self.__syntax.getResponse())
+        response = self.__syntax.getResponse()
+        if not response:
+            return '¡Ups! No te entendí, pregúntame de nuevo.'
+        return self.__request.new(response)
